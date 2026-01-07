cc2ad3380ff84acbdb6afc5e977039586d4e42be
Santos Palma <tiagosantos.mr@gmail.com>
2026-01-07 18:07:16 -0300

    searchByName, com JPQL

--- src/main/java/com/estudotiago/dscommerce/controllers/ProductController.java
+++ src/main/java/com/estudotiago/dscommerce/controllers/ProductController.java
@@ -7,14 +7,7 @@ import org.springframework.http.ResponseEntity;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.estudotiago.dscommerce.dto.ProductDTO;
@@ -24,7 +17,7 @@ import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
-public class ProductController {
+public class  ProductController {
 
 	@Autowired
 	private ProductService service;
@@ -37,8 +30,8 @@ public class ProductController {
 	}
 
 	@GetMapping
-	public ResponseEntity<Page<ProductDTO>> findByAll(Pageable pageable) {
-		Page<ProductDTO> dto = service.findAll(pageable);
+	public ResponseEntity<Page<ProductDTO>> findByAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+		Page<ProductDTO> dto = service.findAll(name, pageable);
 		return ResponseEntity.ok(dto);
 
 	}
--- src/main/java/com/estudotiago/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/estudotiago/dscommerce/repositories/ProductRepository.java
@@ -1,10 +1,17 @@
 package com.estudotiago.dscommerce.repositories;
 
+import com.estudotiago.dscommerce.dto.ProductDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.estudotiago.dscommerce.entities.Product;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 	
 
+            @Query("SELECT obj FROM Product obj " +
+                    "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%', :name, '%'))")
+            Page<Product> searchByName(String name, Pageable pageable);
 }
